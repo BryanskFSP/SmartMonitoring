d11a76301486d2792cd68f7512f8989ab57ae4c7
Nikita <nik-801@yandex.ru>
2023-11-03 16:46:16 +0300

    fix

--- Shared/EditModels/TelegramUserEditModel.cs
+++ Shared/EditModels/TelegramUserEditModel.cs
@@ -4,5 +4,6 @@ public class TelegramUserEditModel
 {
     public int TelegramID { get; set; }
     public bool NotificationStatus { get; set; } = true;
+    public Guid? OrganizationID { get; set; }
     public string? MetaInfo { get; set; }
 }
