428280481b01063dae8a475ff9f4356a8bfad446
Nikita <nik-801@yandex.ru>
2023-11-03 16:28:41 +0300

    fixes

---
+++ Server/Migrations/20231103132633_InitializeMigration.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartMonitoring.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitializeMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Organizations",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Organizations", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Admins",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    OrganizationID = table.Column<Guid>(type: "uuid", nullable: false),
+                    Login = table.Column<string>(type: "text", nullable: false),
+                    PasswordHash = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Admins", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Admins_Organizations_OrganizationID",
+                        column: x => x.OrganizationID,
+                        principalTable: "Organizations",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DataBases",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    Host = table.Column<string>(type: "text", nullable: false),
+                    Database = table.Column<string>(type: "text", nullable: false),
+                    User = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    OrganizationID = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DataBases", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_DataBases_Organizations_OrganizationID",
+                        column: x => x.OrganizationID,
+                        principalTable: "Organizations",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Invites",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uuid", nullable: false),
+                    Code = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    ExpireAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    OrganizationID = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Invites", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Invites_Organizations_OrganizationID",
+                        column: x => x.OrganizationID,
+                        principalTable: "Organizations",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Logs",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    OrganizationID = table.Column<Guid>(type: "uuid", nullable: true),
+                    Action = table.Column<int>(type: "integer", nullable: false),
+                    LogType = table.Column<int>(type: "integer", nullable: false),
+                    Entity = table.Column<string>(type: "text", nullable: true),
+                    EntityID = table.Column<string>(type: "text", nullable: true),
+                    EntityJSON = table.Column<string>(type: "text", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Logs", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Logs_Organizations_OrganizationID",
+                        column: x => x.OrganizationID,
+                        principalTable: "Organizations",
+                        principalColumn: "ID");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TelegramUsers",
+                columns: table => new
+                {
+                    TelegramID = table.Column<int>(type: "integer", nullable: false),
+                    NotificationStatus = table.Column<bool>(type: "boolean", nullable: false),
+                    MetaInfo = table.Column<string>(type: "text", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    LastUseAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    OrganizationID = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TelegramUsers", x => x.TelegramID);
+                    table.ForeignKey(
+                        name: "FK_TelegramUsers_Organizations_OrganizationID",
+                        column: x => x.OrganizationID,
+                        principalTable: "Organizations",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Admins_OrganizationID",
+                table: "Admins",
+                column: "OrganizationID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DataBases_OrganizationID",
+                table: "DataBases",
+                column: "OrganizationID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Invites_Code",
+                table: "Invites",
+                column: "Code",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Invites_OrganizationID",
+                table: "Invites",
+                column: "OrganizationID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Logs_OrganizationID",
+                table: "Logs",
+                column: "OrganizationID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TelegramUsers_OrganizationID",
+                table: "TelegramUsers",
+                column: "OrganizationID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TelegramUsers_TelegramID",
+                table: "TelegramUsers",
+                column: "TelegramID",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Admins");
+
+            migrationBuilder.DropTable(
+                name: "DataBases");
+
+            migrationBuilder.DropTable(
+                name: "Invites");
+
+            migrationBuilder.DropTable(
+                name: "Logs");
+
+            migrationBuilder.DropTable(
+                name: "TelegramUsers");
+
+            migrationBuilder.DropTable(
+                name: "Organizations");
+        }
+    }
+}
